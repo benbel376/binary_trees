af3584c100432ec2e2614c4c91d641a6232fd7dc
Biniyam Belayneh <biniyambelayneh376@gmail.com>
2021-12-02 20:32:36 +0300

    adding task 4 and 5

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,19 +1,15 @@
-#include <stdlib.h>
 #include "binary_trees.h"
-/*
- * binary_tree_is_leaf - check if a node is a leaf
- * @node: the node to be checked.
- * Return: 1 or 0
- */
 
+/**
+ * binary_tree_is_leaf - Checks if a node is a leaf.
+ * @node: A pointer to the node to be checked.
+ *
+ * Return: 1. if Leaf, Otherwise - 0.
+ */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-if(node->left == NULL && node->right == NULL)
-{
-return 1;
-}
-else
-{
-return 0;
-}
+	if (node == NULL || node->left != NULL || node->right != NULL)
+		return (0);
+
+	return (1);
 }
--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,15 +1,15 @@
-#include <stdlib.h>
 #include "binary_trees.h"
-/*
- *
- *
- *
- */
 
+/**
+ * binary_tree_is_root - Checks if a node is a roote.
+ * @node: A pointer to the node to be checked.
+ *
+ * Return: If root 1. Otherwise - 0.
+ */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-if(node->parent == NULL)
-return 1;
-else
-return 0;
+	if (node == NULL || node->parent != NULL)
+		return (0);
+
+	return (1);
 }
